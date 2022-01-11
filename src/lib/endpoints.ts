a44f280a1f334d42d6af6129c08e7f0d73a8c6ea
Daniel Imfeld <daniel@imfeld.dev>
2022-01-10 22:26:45 -1000

    Prepare for JSON and traditional form handlers

--- src/lib/endpoints.ts
+++ src/lib/endpoints.ts
@@ -1,6 +1,9 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { DefaultBody } from '@sveltejs/kit/types/endpoint';
+import { ReadOnlyFormData } from '@sveltejs/kit/types/helper';
+import { ServerRequest, ServerResponse } from '@sveltejs/kit/types/hooks';
 import { Theme } from './styles';
+import Accepts from 'accepts';
 
 export type TracksomeLocals<Authed extends boolean = true> = {
   userId: Authed extends true ? number : number | null;
@@ -13,7 +16,7 @@ type Typify<T> = { [K in keyof T]: Typify<T[K]> };
 
 type TracksomeRequestHandler<Input = unknown, Output = DefaultBody> = RequestHandler<
   TracksomeLocals,
-  Input,
+  Input | ReadOnlyFormData,
   Typify<Output>
 >;
 export type { TracksomeRequestHandler as RequestHandler };
@@ -24,4 +27,18 @@ export function parseNumber(value: string | null): number | undefined {
     return undefined;
   }
   return v;
+}
+
+export function responseAccepts(
+  request: ServerRequest,
+  responses: Record<string, () => ServerResponse | Promise<ServerResponse>>
+): ServerResponse | Promise<ServerResponse> {
+  // @ts-ignore Different types but they overlap in the proper places.
+  let accepts = new Accepts(request);
+
+  let acceptedTypes = Object.keys(responses).filter((t) => t !== 'default');
+
+  let contentType = accepts(acceptedTypes) || 'default';
+
+  return responses[contentType]();
 }
