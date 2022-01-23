0b0c77cf31a3a92609bfe12304f2f6fd56275d87
Daniel Imfeld <daniel@imfeld.dev>
2022-01-22 17:45:26 -1000

    Fix browser not liking <div> inside <p>

--- src/lib/components/Switch.svelte
+++ src/lib/components/Switch.svelte
@@ -5,14 +5,14 @@
 
 <label class="relative inline-flex items-center">
   <input type="checkbox" {name} class="w-0 h-0 opacity-0" bind:checked={value} />
-  <div
+  <span
     class="slider-bg inset-0 inline-flex flex-shrink-0 bg-gray-200 dark:bg-gray-600 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 ring-offset-dwhite"
     class:mr-2={$$slots.default}
   >
     <span
       class="slider pointer-events-none inline-block cursor-pointer bg-white dark:bg-gray-900 h-5 w-5 rounded-full shadow ring-0 transition ease-in-out duration-200"
     />
-  </div>
+  </span>
   <slot />
 </label>
 
